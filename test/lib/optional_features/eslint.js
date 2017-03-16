404918b2bd78c7a4ae47630f15533c948c53eb6c
zhonghua <zhonghua@cd.tuan800.com>
2017-03-16 20:14:53 +0800

    调整eslint结构

--- test/lib/optional_features/eslint.js
+++ test/lib/optional_features/eslint.js
@@ -17,12 +17,14 @@ describe('eslint feature', function() {
   });
 
   describe('#update', function() {
+    let fakeGitHooksDir = 'dotgit/hooks';
     it('.git/hooks/pre-commit should symbol link to bin/pre-commit', sinon.test(function(done) {
       let eslintFeature = require(path.join(env.koa800Root, 'lib/optional_features/eslint.js'));
       this.stub(env.Feature, 'getUpdate').returns({eslint: eslintFeature.update});
+      this.stub(env.Feature, 'gitHooksDir').returns(fakeGitHooksDir);
 
       env.Feature.makeScaffold(env.testScaffoldRoot).then(function() {
-        require('fs').readlink(path.join(env.testScaffoldRoot, '.git/hooks/pre-commit'), function(err, target) {
+        require('fs').readlink(path.join(env.testScaffoldRoot, `${fakeGitHooksDir}/pre-commit`), function(err, target) {
           if (err) return done(err);
           expect(target).to.equal(path.join(env.koa800Root, 'bin/pre-commit'));
           done();
@@ -33,7 +35,7 @@ describe('eslint feature', function() {
     after(function() { // TODO
       require('rimraf').sync(path.join(env.testScaffoldRoot, 'app'));
       require('rimraf').sync(path.join(env.testScaffoldRoot, 'config'));
-      require('fs').unlinkSync(path.join(env.testScaffoldRoot, '.git/hooks/pre-commit'));
+      require('fs').unlinkSync(path.join(env.testScaffoldRoot, `${fakeGitHooksDir}/pre-commit`));
     });
   });
 });
